6a69c33bf73c55aa871ccbbce974339c1a660f76
Taytum <twisstr@g.crookcountyschools.org>
2022-06-01 08:41:56 -0700

    labs 11&12 front end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,67 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel, Button } from 'react-bootstrap';
+import bookImg from './bookimage.jpeg';
+import Container from "react-bootstrap/Container";
+import BookFormModal from './BookformModal';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      showForm: false
     }
   }
 
+
+
+
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount = async () => {
+    console.log('function fired off');
+    try {
+      const url = `http://localhost:3001/books`;
+      const booksResponse = await axios.get(url);
+      console.log(booksResponse.data);
+      this.setState({ books: booksResponse.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+deleteBook = async (bookToBeDeleted) => {
+  try {
+const proceed = window.confirm(`Do you want to delete ${bookToBeDeleted.title}?`);
+  
+if (proceed) {
+  let newBooks = this.state.books.filter(book => book._id !== bookToBeDeleted._id);
+  this.setState({books: newBooks});
+  
+  const config = {
+    method: "delete",
+    baseURL: process.env.REACT_APP_SERVER,
+    url: `/books/${bookToBeDeleted._id}`
+  };
+  await axios(config);
+}
+} catch (error) {
+console.error(error);
+  }
+}
+  handleClose = () => this.setState({ showForm: false })
+
+
+  createBook = async newBook => {
+    const config = {
+      method: "post",
+      baseURL: process.env.REACT_APP_SERVER,
+      url: "/books/",
+      data: newBook
+    };
+    const bookResults = await axios(config);
+    const updatedBooks = [...this.state.books, bookResults.data];
+    this.setState({books : updatedBooks})
+  }
 
   render() {
 
@@ -17,9 +70,32 @@ class BestBooks extends React.Component {
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <Button onClick={() => this.setState({ showForm: true })}>
+          add a book
+        </Button>
+        {this.state.showForm && <BookFormModal handleClose={this.handleClose} show={this.state.showForm} createBook = {this.createBook}/>}
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <Container >
+            <Carousel>
+              {this.state.books.map(book => (
+                <Carousel.Item key={book._id}>
+                  <img
+                    id='carousel-image'
+                    className="d-block w-100"
+                    src={bookImg}
+                    alt={book.title}
+                  />
+
+                  <Carousel.Caption id='carousel-text-box'>
+                    <h3>{book.title}</h3>
+                    <p>{book.description}</p>
+                    <p>Status: {book.status}</p>
+                  <Button onClick= {() => this.deleteBook(book)}>Delete</Button>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Container>
         ) : (
           <h3>No Books Found :(</h3>
         )}
